41c4363791231708caa49ad86305aa9a70968ab2
tikvasonnenberg <tikisonnenberg@gmail.com>
2020-06-30 12:03:12 +0300

    Add files via upload

---
+++ Direction-Arduino/Direction.ino
@@ -0,0 +1,24 @@
+
+#include "Encoder_Polling.h"
+
+const int encoderPin_A = 8;
+const int encoderPin_B = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+  encoder_begin(encoderPin_A, encoderPin_B); // Start the decoder
+}
+
+void loop()
+{
+  int dir = encoder_data(); // Check for rotation
+  
+  if(dir != 0)              // If it has rotated...
+  {
+      Serial.println(dir);   // Print the direction
+      Serial.write(dir);
+      Serial.flush();
+      delay(100);
+  }  
+}
